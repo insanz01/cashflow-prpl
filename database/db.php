71eb2c0fc42926cdc679f0381cfa756e6b12ebd9
insan kamil <insankamil002@gmail.com>
2019-04-05 20:01:13 +0700

    first commit

---
+++ database/db.php
@@ -0,0 +1,8 @@
+<?php
+$username = "root";
+$database = "nama_database";
+$password = "";
+$server = "localhost";
+
+$c = mysqli_connect($server, $username, $password, $database) or die("Koneksi bermasalah");
+
